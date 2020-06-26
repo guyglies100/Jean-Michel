19e2defd2a010ee69e7fee20d3e15c6af04c0d64
GuiD <guyglies100@gmail.com>
2020-06-26 14:24:44 -0400

    weekly add

--- dashboard.py
+++ dashboard.py
@@ -14,14 +14,28 @@ def time_delta_format(timedelta):
 
 	return "Heures: " + str(hours) + ", Minutes: " + str((timedelta.seconds//60)%60) + ", Moyenne par semaine: " + str(round(hours/weeks_since_semester_start, 2))
 
-def print_dict(dictionary):
+def print_dict(dictionary, weekly):
 	print_str = []
 	for key in dictionary:
-		print_str.append(str(key)+' -> '+time_delta_format(dictionary[key]))
+		string = str(key)+' -> '+time_delta_format(dictionary[key])
+		if(key in weekly):
+			timedelta = weekly[key]
+			hours = timedelta.days * 24 + timedelta.seconds//3600
+			minutes = (timedelta.seconds//60)%60
+			string += ", Depuis mercredi: " + str(hours) + "h" + str(minutes) + "min"
+		print_str.append(string)
 	return print_str
+
+def get_last_wednesday():
+	today = datetime.today()
+	offset = (today.weekday() - 2) % 7
+	last_wednesday = today - timedelta(days=offset)
+	last_wednesday = datetime(year = last_wednesday.year, month = last_wednesday.month, day = last_wednesday.day)
+	return last_wednesday
 
 def dashboard():
 	work_time_dictionary  = dict()
+	work_time_weekly_dictionary  = dict()
 	f = open("./Persistence/Work.csv","r")
 	lines = f.readlines()[1:]
 	for line in lines:
@@ -31,4 +45,14 @@ def dashboard():
 		else:
 			work_time_dictionary[array[0]] = getTimeObj(array[2]) - getTimeObj(array[1])
 
-	return print_dict(work_time_dictionary)
+		if(getTimeObj(array[1]) > get_last_wednesday()):
+			if array[0] in work_time_weekly_dictionary:
+				work_time_weekly_dictionary[array[0]] += getTimeObj(array[2]) - getTimeObj(array[1])
+			else:
+				work_time_weekly_dictionary[array[0]] = getTimeObj(array[2]) - getTimeObj(array[1])
+		elif(getTimeObj(array[1]) < get_last_wednesday() and getTimeObj(array[2]) > get_last_wednesday()):
+			if array[0] in work_time_weekly_dictionary:
+				work_time_weekly_dictionary[array[0]] += getTimeObj(array[2]) - get_last_wednesday()
+			else:
+				work_time_weekly_dictionary[array[0]] = getTimeObj(array[2]) - get_last_wednesday()
+	return print_dict(work_time_dictionary, work_time_weekly_dictionary)
